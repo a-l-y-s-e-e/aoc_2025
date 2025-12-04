6a8ce8bb02225e5f01879fbe28556f056aad08eb
Pol Chapon <pchapon@amazon.com>
2025-12-04 09:50:21 +0000

    Day 3 solution

---
+++ 3/p.py
@@ -0,0 +1,40 @@
+import copy
+import sys
+from pathlib import Path
+import re
+#########################################
+
+
+# Defining main function
+def main():
+    with (open('/Users/pchapon/projects/aoc/2025/3/input.txt') as f):
+        lines = f.read().split("\n")
+        print(lines)
+        res = 0
+        for l in lines:
+            print('-----')
+            if l == '':
+                break
+            print(l)
+            max_l = l[0]
+            max_l_i = 0
+            for i in range(len(l)-1):
+                if max_l < l[i]:
+                    max_l = l[i]
+                    max_l_i = i
+            max_r = l[-1]
+            max_r_i = len(l)-1
+            for i in range(len(l)-2, max_l_i, -1):
+                if max_r < l[i]:
+                    max_r = l[i]
+                    max_r_i = i
+            print(max_l+max_r)
+            res += int(max_l+max_r)
+        print('result:', res)
+
+
+
+
+#########################################
+if __name__=="__main__":
+    main()
---
+++ 3/p_.py
@@ -0,0 +1,42 @@
+import copy
+import sys
+from pathlib import Path
+import re
+#########################################
+
+
+def get_max(l, iter):
+    if iter <= 0:
+        return ''
+    #print('get_max', l, iter)
+    max_l = l[0]
+    max_l_i = 0
+    for i in range(len(l)-iter+1):
+        if max_l < l[i]:
+            max_l = l[i]
+            max_l_i = i
+    #print(max_l, max_l_i)
+    return max_l + get_max(l[max_l_i+1:], iter-1)
+
+# Defining main function
+def main():
+    with (open('/Users/pchapon/projects/aoc/2025/3/input.txt') as f):
+        lines = f.read().split("\n")
+        print(lines)
+        res = 0
+        for l in lines:
+            print('-----')
+            if l == '':
+                break
+            print(l)
+            m = int(get_max(l, 12))
+            print(m)
+            res += m
+        print('result:', res)
+
+
+
+
+#########################################
+if __name__=="__main__":
+    main()
