b3de3baa74d8dc2fa089a2f1d5870689ec5d8f08
Pol Chapon <pchapon@amazon.com>
2025-12-05 11:55:48 +0000

    Day 5 part 1 solution

---
+++ 5/part_2.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+#########################################
+class Sol:
+    BASE_DIR = Path(__file__).parent.resolve()
+
+    def count_adjacent(self, r, c):
+        total = 0
+        for dr in [-1, 0, 1]:
+            for dc in [-1, 0, 1]:
+                if not (dr==0 and dc==0):
+                    if -1 < dr+r < self.R and -1 < dc+c < self.C and self.grid[dr+r][dc+c] == '@':
+                        total += 1
+        return total
+
+    def main(self):
+        with (open(os.path.join(self.BASE_DIR, 'input.txt')) as f):
+            self.grid = f.read().split("\n")
+            self.grid = [list(a) for a in self.grid]
+            self.grid = self.grid[:-1]
+            print(self.grid)
+            self.R = len(self.grid)
+            self.C = len(self.grid[0])
+            res = 0
+            modified = 1
+            while modified > 0:
+                modified = 0
+                for r in range(self.R):
+                    for c in range(self.C):
+                        if self.grid[r][c] == '@' and self.count_adjacent(r, c) < 4:
+                            self.grid[r][c] = '.'
+                            res += 1
+                            modified += 1
+            print('result:', res)
+
+#########################################
+if __name__=="__main__":
+    sol = Sol()
+    sol.main()
